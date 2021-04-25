7b56ec7e634ab25ff2356b3dd9ddc0a801a49293
Retkid <54078872+Retkid@users.noreply.github.com>
2021-04-24 22:47:30 -0400

    Updated ascii.sh to actually function

--- ascii.sh
+++ ascii.sh
@@ -13,18 +13,19 @@ if [[ ${1} == 'id' ]]; then
     done    
     echo "1" > cache.txt
 elif [[ ${1} == 'convert' ]]; then
+    sed -i "${6}s"'/.*/ 0 /' cache.txt
     i=$2
     while [ $i -le "$3" ]; do
     convert ${4}/noprocess${i}.jpg -crop 1500x1500+530+280 -type Grayscale processed/${5}/rank/rank${i}.jpg
     i=$(($i + 1))
     done
-    sed -i "${6}i/anything" cache.txt
+    sed -i "${6}s"'/.*/1 /' cache.txt
 else
-    cat "${1}/source.txt"|
+    ls ${2}/*.jpg|
     while read line; do
         line1=${line##*/}
         line1=${line1%.*}
-        jp2a ${line} --size=100x40 --chars=01 --html --output=${1}/${line1}.html
+        jp2a ${line} --size=100x40 --chars=01 --html --output=${1}/html/${line1}.html
         done
     if [[ $(which figlet 2> /dev/null) ]]; then
         figlet '* done *'
