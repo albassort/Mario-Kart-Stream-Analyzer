ecf88e0bad11fde3260c5306da7b2699add5a93a
Retkid <54078872+Retkid@users.noreply.github.com>
2021-04-24 22:41:32 -0400

    Fixed a bunch of bugs and updated with the rest of project.
    this file was neglected and was not functional, and now it works!

--- todo.py
+++ todo.py
@@ -1,10 +1,10 @@
-from init import read, write, linecount, ocr_core  # type: ignore
+from init import read, write, linecount, ocr_core, writelist  # type: ignore
 import os
 import sys
 import time
 from pixget import getplayers  # type: ignore
 from multiprocessing import Process
-from stopwatch import Stopwatch
+from stopwatch import Stopwatch as stopwatch
 import cv2
 from PIL import Image
 import shutil
@@ -13,10 +13,11 @@ from asciidetection import gen, asciimain  # type: ignore
 def penme(todo):
     print('testt')
     cc = []
-    v1 = len(os.listdir(todo))-2
+    v1 = len(os.listdir(todo))-3
     v1 = (int(v1/4), int(v1)%4)
     #counts the amount of files in todo and creates 1/4 slices for
     state = []
+    print(v)
     if v != 0:
         print('test')
         #detects if the images are already genered
@@ -28,16 +29,22 @@ def penme(todo):
                 shutil.rmtree(f'processed/{x}')
             if match == todo:
                 state.append(1)
-                match = x
+                globals()['v'] = x
                 break
             else:
                 state.append(0)
         print(state)
         print(x)
+    else:
+        state.append(0) 
     if state[-1] == 0:
         #ugly ugly subprocess code
         print(state[-1])
         os.makedirs(f'processed/{v}/rank')
+        if os.path.exists('cache.txt') == False:
+            with open('cache.txt', 'w') as temp:
+                temp.close
+            write("0", 3, 'cache.txt')
         #does some basic math to allocate the 4 subprocess range
         #to perfectly split up 'todo ls \ wc -l'
         subprocess.Popen(['bash', 'ascii.sh', 'convert',  str(1), str(v1[0]), todo, str(v), str(1)])
@@ -49,11 +56,13 @@ def penme(todo):
         #the weird check for completion of subprocess.
         #reads cache.txt and awaits for the day that the subprocces's are finished.
         while len(cc) != 4:
-            for x in range(1, 4):
+            for x in range(0, 4):
                 time.sleep(1)
                 #stop the read from breaking my disk's iowait lol.
-                cc1 = read(x, 'cache.txt')
-                if cc1 != "1":
+                temp = read(x, 'cache.txt')
+                if len(cc) >= 4:
+                    break
+                if temp != "1":
                     continue
                 else:
                     cc.append(1)
@@ -75,12 +84,14 @@ def penme(todo):
         print('beep')
     #generates ascii rank-detection.
   #  writelist(os.listdir(f'processed/{v}/id'), 0, f'{exdir}/source.txt')
-    gen('regen', exdir)
+    os.makedirs(f'{exdir}/html')
+    print(f'processed/{v}')
+    gen('gen', exdir, f'processed/{v}')
     asciimain(exdir)
 
 count = linecount('todo.txt')
 for xz in range(0, count):
-    todo = read(xz, 'todo.txt')
+    todo = read(xz, 'todo.txt')[:-1]
     if os.path.exists("processed/") == False:
         os.mkdir('processed/')
     v = len(os.listdir('processed/'))
@@ -91,6 +102,7 @@ for xz in range(0, count):
     elif todo == 'quit':
         quit()
     else:
+        s = stopwatch()
         if os.path.exists(f'{todo}/temp/temp50.jpg') == False:
             write(f'done, {todo}//ERROR MISSING temp50.jpg', xz, 'todo.txt')
             print('ERROR MISSING TEMP50')
@@ -98,17 +110,15 @@ for xz in range(0, count):
         #this is pretty self explanitory
         #this just takes stuff from around the files and gleans all the data it can from it
         channel = todo.partition('/')[0]
+        if os.path.exists(f'{channel}/out/') == False:
+            os.makedirs(f'{channel}/out/')
         z = len(os.listdir(f'{channel}/out'))
         exdir = f'{channel}/out/{z}'
         print('test pp')
         penme(todo)
         subprocess.Popen(
-            ['convert', f'{todo}/temp/temp50.jpg', '-crop 640x360+210+315', '-crop', '-200-20', '-type Grayscale', '-sharpen 0x12', f'{exdir}/boop.jpg'])
-
-        shutil.move(f'  {todo}/temp/temp50.jpg', exdir)
-        ai = ocr_core(f'{exdir}/boop.jpg')
-        print(ai)
-        write(ai, 0, f'{exdir}/meta.txt')
+            ['convert', f'{todo}/temp/temp50.jpg', '-crop', '640x360+210+315', '-crop', '-200-20', '-type', 'Grayscale', '-sharpen', '0x12', f'{exdir}/boop.jpg'])
+        shutil.copy(f'{todo}/temp/temp50.jpg', exdir)
         players = getplayers(f'{todo}/temp/temp50.jpg')
         write(players, 1, f'{exdir}/meta.txt')
         zf = linecount(f'{exdir}/rank.txt')
@@ -128,6 +138,10 @@ for xz in range(0, count):
         write(f'processed/{v}', 11, f'{exdir}/meta.txt')
         time.sleep(1)
         write(f'done, {todo}', xz, 'todo.txt')
+        print(s.stop())
+        ai = ocr_core(f'{exdir}/boop.jpg')
+        write(ai, 0, f'{exdir}/meta.txt')
+
 #line 1 = Stage
 # 2 = players
 # 3 = final rank entry
